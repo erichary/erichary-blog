0ef1011627c3298242237fb86a5218794332a656
erichary <1326330617@qq.com>
2020-07-21 23:18:21 +0800

    项目创建

---
+++ src/main/java/com/erichary/blog/EricharyBlogApplication.java
@@ -0,0 +1,18 @@
+package com.erichary.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 程序入口
+ *
+ * @author erichary
+ */
+@SpringBootApplication
+public class EricharyBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EricharyBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/erichary/blog/EricharyBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erichary.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EricharyBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
